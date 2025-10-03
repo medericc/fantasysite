ac8b129a8ddbc87c8c6e1288414df2cb411e79bb
medericc <bleublanctour1@>
2025-10-03 04:30:48 +0200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
-
+import { Analytics } from "@vercel/analytics/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -85,6 +85,7 @@ export default function RootLayout({
             © {new Date().getFullYear()} First Pick – Média basket LFB & LF2
           </div>
         </footer>
+        <Analytics />
       </body>
     </html>
   )

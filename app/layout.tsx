62a40b7ee998de8331e65c1ae203e2f460acec98
medericc <bleublanctour1@>
2026-09-17 05:31:02 +0200

    ee

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,7 +76,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
-     <head><GoogleTagManager gtmId="GTM-MXF5G4KH" /> </head>
+     <head><GoogleTagManager gtmId="GTM-MXF5G4KH" /> <meta name="google-site-verification" content="Su4aAudUBo8Tx6-mEcQX1fo0_Ore_5ZI4inSPwZEAgM" /> </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-slate-50 dark:bg-slate-900`}
       >
